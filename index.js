2895e64534bb5fb977b1d0e142e7011357754d0d
Pin <pin@climax.com.tw>
2024-09-12 10:43:33 +0800

    feat-002

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const fs = require('fs');
 try {
   const data = fs.readFileSync('./account.json', 'utf8');
 
-  if (data.username === "john") {
-    console.log("not pin")
-  }
+  const user = JSON.parse(data)
+
+  console.log(user.info.age)
+  console.log(user.info.habbit)
 
 } catch (err) {
   console.error(err);
